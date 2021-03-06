0a368b8e3e8b1e4a6ecd1ac4337d3af4b6df7bb7
Nick Howell <howellnick@gmail.com>
2021-03-05 23:37:39 -0500

    Add react-query and display the game status
    Also remove default React stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <h1>SpaceTraders</h1>
+      <GameStatus />
+    </QueryClientProvider>
+  );
+}
+
+function GameStatus() {
+  const { status, data, error } = useGameStatus();
+
+  return status === "loading" ? (
+    <span>"Loading..."</span>
+  ) : status === "error" ? (
+    <span>
+      <strong>Error:</strong> {error?.status}
+    </span>
+  ) : (
+    <span>
+      <strong>Status:</strong> {data?.status}
+    </span>
+  );
+}
+
+interface IGameStatusResponse {
+  status: string;
+}
+
+function useGameStatus() {
+  return useQuery<{}, IGameStatusResponse, IGameStatusResponse>(
+    "game-status",
+    async () => {
+      const response = await fetch("https://api.spacetraders.io/game/status");
+      return await response.json();
+    }
   );
 }
 
