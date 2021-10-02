1891b9b437e1525c6503f456561b35083326aa75
Nick Howell <howellnick@gmail.com>
2021-10-01 20:52:12 -0400

    Remove some code that's no longer needed since moving to the new endpoints

--- src/spacetraders-api/hooks/my/ships/useMyShips.ts
+++ src/spacetraders-api/hooks/my/ships/useMyShips.ts
@@ -19,61 +19,15 @@ export function useMyShips() {
 	);
 }
 
-export function setShipsQueryData(ships: IMyShip[]) {
-	let shouldInvalidateUserShips = false;
-
-	spaceTradersQueryClient.setQueryData<IGetMyShipsResponse>(
-		MY_SHIPS_QUERY_KEY,
-		(old) => {
-			const newShips = produce(ships, (draft) => {
-				if (old !== undefined) {
-					for (const ship of draft) {
-						if (
-							ship.location === undefined &&
-							ship.flightPlanId === undefined
-						) {
-							// The get user info endpoint returns the ships without
-							// their flight plan IDs, so we want to make sure we don't lose the
-							// flight plan IDs if we already have them.
-							const previousShip = old.ships.find((x) => x.id === ship.id);
-							if (
-								previousShip !== undefined &&
-								previousShip.flightPlanId !== undefined
-							) {
-								ship.flightPlanId = previousShip.flightPlanId;
-							}
-						}
-					}
-				}
-
-				// If a ship doesn't have either a location or a flightPlanId, then we
-				// will have to fetch the data again properly.
-				if (
-					draft.some(
-						(x) => x.location === undefined && x.flightPlanId === undefined,
-					)
-				) {
-					shouldInvalidateUserShips = true;
-				}
-			});
-
-			updateRelatedQueryData(newShips);
-
-			return { ships: newShips };
-		},
-	);
-
-	if (shouldInvalidateUserShips) {
-		spaceTradersQueryClient.invalidateQueries(MY_SHIPS_QUERY_KEY, {
-			exact: true,
-		});
-	}
-}
-
 export function setShipInShipsQueryData(ship: IMyShip) {
-	const data = spaceTradersQueryClient.getQueryData<IGetMyShipsResponse>(
-		MY_SHIPS_QUERY_KEY,
-	) ?? { ships: [] };
+	const data =
+		spaceTradersQueryClient.getQueryData<IGetMyShipsResponse>(
+			MY_SHIPS_QUERY_KEY,
+		);
+
+	if (data === undefined) {
+		return;
+	}
 
 	const newData = produce(data, (draft) => {
 		const index = draft.ships.findIndex((x) => x.id === ship.id);
