e8ec770320da53b77868c4dc160231bda71fe7a8
Nick Howell <howellnick@gmail.com>
2021-10-02 22:56:46 -0400

    Cache system info forever
    The only information in the system info is it's name, which isn't likely to change.

--- src/spacetraders-api/hooks/systems/useSystemInfo.ts
+++ src/spacetraders-api/hooks/systems/useSystemInfo.ts
@@ -5,7 +5,12 @@ import { systemQueryKey } from ".";
 export function useSystemInfo(systemSymbol: string) {
 	const spaceTradersApi = useSpaceTradersApi();
 
-	return useSpaceTradersQuery(systemQueryKey(systemSymbol), () =>
-		spaceTradersApi.systems.getSystemInfo(systemSymbol),
+	return useSpaceTradersQuery(
+		systemQueryKey(systemSymbol),
+		() => spaceTradersApi.systems.getSystemInfo(systemSymbol),
+		{
+			cacheTime: Infinity,
+			staleTime: Infinity,
+		},
 	);
 }
