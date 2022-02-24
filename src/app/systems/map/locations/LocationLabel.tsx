1e89df97a944bbbbdaa378187a0b6a010b5b8744
Nick Howell <howellnick@gmail.com>
2022-02-24 00:00:45 -0500

    Make the location labels face the camera

--- src/app/systems/map/locations/LocationLabel.tsx
+++ src/app/systems/map/locations/LocationLabel.tsx
@@ -1,6 +1,8 @@
-import { ReactElement } from "react";
+import { ReactElement, useRef } from "react";
 import { Html } from "@react-three/drei";
 import { Tag } from "../../../common/Tag";
+import { useFrame } from "@react-three/fiber";
+import { Mesh } from "three";
 
 interface IOwnProps {
 	radius: number;
@@ -9,16 +11,18 @@ interface IOwnProps {
 }
 
 export function LocationLabel(props: IOwnProps): ReactElement {
+	// This reference will give us direct access to the mesh
+	const mesh = useRef<Mesh>(null!);
+
+	useFrame(({ camera }) => {
+		// Make text face the camera
+		mesh.current.quaternion.copy(camera.quaternion);
+	});
+
 	return (
-		<mesh>
-			<Html
-				scale={0.7}
-				rotation={[0, 0, 0]}
-				position={[0, props.radius + 0.7, 0]}
-				transform
-				occlude
-			>
-				<h2 style={{ textAlign: "center" }}>
+		<mesh ref={mesh}>
+			<Html scale={0.7} position={[0, props.radius + 1, 0]} transform occlude>
+				<h2 style={{ textAlign: "center", margin: 0 }}>
 					<Tag text={props.symbol} />
 					<br />
 					{props.name}
