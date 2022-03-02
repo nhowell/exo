747a8517c0581cc641f97329f94d9cd0e22fc550
Nick Howell <howellnick@gmail.com>
2022-03-01 22:53:06 -0500

    Add system nav and system name to the full page system map

--- src/app/systems/ViewSystemMap.tsx
+++ src/app/systems/ViewSystemMap.tsx
@@ -1,8 +1,12 @@
 import { ReactElement } from "react";
 import { useParams } from "react-router";
+import { NavLink } from "react-router-dom";
 import { useSystemInfo } from "../../spacetraders-api/hooks/systems/useSystemInfo";
+import { generateViewSystemMapPath } from "../routes";
 import { SystemMap } from "./map/SystemMap";
 import { useAddVisitedSystem } from "./useAddVisitedSystem";
+import { useKnownSystems } from "./useKnownSystems";
+import styles from "./ViewSystemMap.module.css";
 
 export function ViewSystemMap(): ReactElement {
 	const { systemSymbol } = useParams();
@@ -15,5 +19,29 @@ export function ViewSystemMap(): ReactElement {
 
 	useAddVisitedSystem(result.data?.system.symbol);
 
-	return <SystemMap systemSymbol={systemSymbol} />;
+	const knownSystems = useKnownSystems();
+
+	return (
+		<>
+			<header className={styles.header}>
+				{knownSystems.data.length > 1 && (
+					<nav>
+						{knownSystems.data.map((system) => (
+							<NavLink
+								key={system}
+								to={generateViewSystemMapPath(system)}
+								className={({ isActive }) => (isActive ? styles.active : "")}
+								end
+							>
+								{system}
+							</NavLink>
+						))}
+					</nav>
+				)}
+				<h1>{result.data?.system.name}</h1>
+			</header>
+
+			<SystemMap systemSymbol={systemSymbol} />
+		</>
+	);
 }
