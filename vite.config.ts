534ccc5d8871a1ee6f42a258b2d2bbe3b6a4331d
Nick Howell <howellnick@gmail.com>
2023-02-05 14:43:04 -0500

    Set up tsc and eslint to run automatically

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { checker } from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		checker({
+			eslint: {
+				lintCommand: 'eslint . --ext .ts,.tsx --max-warnings=0"',
+			},
+			typescript: true,
+		}),
+	],
 	server: {
 		open: true,
 		port: 3000,
