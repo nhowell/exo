db2c4821697144feb35b62e33c7f5b129434cfa5
Nick Howell <howellnick@gmail.com>
2023-02-05 15:45:33 -0500

    Switch from Jest to Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { checker } from "vite-plugin-checker";
@@ -7,14 +8,25 @@ export default defineConfig({
 	plugins: [
 		react(),
 		checker({
-			eslint: {
-				lintCommand: 'eslint . --ext .ts,.tsx --max-warnings=0"',
-			},
-			typescript: true,
+			eslint: process.env.VITEST
+				? undefined
+				: {
+						lintCommand: 'eslint . --ext .ts,.tsx --max-warnings=0"',
+				  },
+			typescript: !process.env.VITEST,
 		}),
 	],
 	server: {
 		open: true,
 		port: 3000,
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/setupTests.ts",
+		coverage: {
+			reporter: ["text", "html"],
+			exclude: ["node_modules/", "src/setupTests.ts"],
+		},
 	},
 });
