da910d8cfda75b33afa359230334bcf0a7dd18b4
Nick Howell <howellnick@gmail.com>
2023-02-26 15:33:07 -0500

    Move fuel and docking efficiencies into the same file that they're used

--- src/utils/calculateFuelCostForFlightPlan.ts
+++ src/utils/calculateFuelCostForFlightPlan.ts
@@ -2,9 +2,6 @@ import { ShipType } from "@/spacetraders-api/api/enums";
 import { ILocation } from "@/spacetraders-api/api/locations/types";
 import { IMyDockedShip } from "@/spacetraders-api/api/my/ships/types";
 
-import { shipDockingEfficiencies } from "../app/ships/shipDockingEfficiencies";
-import { shipFuelEfficiencies } from "../app/ships/shipFuelEfficiencies";
-
 import { calculateDistance } from "./calculateDistance";
 import { calculateFuelCost } from "./calculateFuelCost";
 
@@ -53,3 +50,47 @@ function getDockingEfficiency(shipType: ShipType): number {
 
 	return dockingEfficiency;
 }
+
+// See: https://discord.com/channels/792864705139048469/1016976560085024860/1017895369604071557
+const shipFuelEfficiencies = new Map<ShipType, number>([
+	[ShipType.JackshawI, 4],
+	[ShipType.JackshawII, 4],
+	[ShipType.ZetraII, 3],
+	[ShipType.ZetraIII, 3],
+	[ShipType.GravagerI, 4],
+	[ShipType.GravagerII, 4],
+	[ShipType.GravagerIII, 4],
+	[ShipType.ElectrumI, 4],
+	[ShipType.ElectrumII, 3],
+	[ShipType.ElectrumIII, 3],
+	[ShipType.ElectrumIV, 2],
+	[ShipType.HermesI, 3],
+	[ShipType.HermesII, 3],
+	[ShipType.HermesIII, 3],
+	[ShipType.TiddalikI, 3],
+	[ShipType.DradesI, 4],
+	[ShipType.ZatashiI, 2],
+	[ShipType.ZatashiII, 2],
+]);
+
+// See: https://discord.com/channels/792864705139048469/1016976560085024860/1017895369604071557
+const shipDockingEfficiencies = new Map<ShipType, number>([
+	[ShipType.JackshawI, 2],
+	[ShipType.JackshawII, 2],
+	[ShipType.ZetraII, 1],
+	[ShipType.ZetraIII, 1],
+	[ShipType.GravagerI, 2],
+	[ShipType.GravagerII, 3],
+	[ShipType.GravagerIII, 4],
+	[ShipType.ElectrumI, 2],
+	[ShipType.ElectrumII, 1],
+	[ShipType.ElectrumIII, 1],
+	[ShipType.ElectrumIV, 1],
+	[ShipType.HermesI, 1],
+	[ShipType.HermesII, 1],
+	[ShipType.HermesIII, 1],
+	[ShipType.TiddalikI, 4],
+	[ShipType.DradesI, 4],
+	[ShipType.ZatashiI, 1],
+	[ShipType.ZatashiII, 1],
+]);
