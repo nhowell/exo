2c46b33adedc93b0876ae9c6162141a581d659ef
Nick Howell <howellnick@gmail.com>
2023-02-26 11:07:56 -0500

    Add "import/no-default-export" ESLint rule

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { checker } from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line import/no-default-export
 export default defineConfig({
 	plugins: [
 		react(),
